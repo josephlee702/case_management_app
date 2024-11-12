d359b2c03239b36160756b1d825aad3f45d56e56
Joseph Lee <jhjlee702@gmail.com>
2024-11-12 16:36:36 -0700

    create a client

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Client.create(first_name: "Joseph", last_name: "Lee", email: "jhjlee702@gmail.com", phone_number: "123-456-7890", address: "123 Test St", city: "Testing City", state: "Colorado", zip_code: "12345", gender: 1, DOB: Date.new(1997, 7, 2), emergency_contact_name: "Joanna Lee", emergency_contact_phone: "000-000-0000", drivers_license: "9283993205", insurance: "Statefarm", notes: "This is a test client.")

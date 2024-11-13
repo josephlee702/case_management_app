c2859a7abd66e1d2da42fb9b8c7ebe4b4ba14032
Joseph Lee <jhjlee702@gmail.com>
2024-11-12 17:25:26 -0700

    callback to generate random case number upon Case creation

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,4 +1,12 @@
 class Case < ApplicationRecord
   belongs_to :client
   enum case_status: { "Open": 0, "Closed": 1 }
+
+  before_create :generate_case_number
+
+  private
+
+  def generate_case_number
+    self.case_number ||= "#{SecureRandom.hex(5).upcase}"
+  end
 end

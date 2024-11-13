14af375f3dbd6b1bc311b95c391e1e1d9ee4f38b
Joseph Lee <jhjlee702@gmail.com>
2024-11-12 17:06:11 -0700

    add note to case seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,4 @@
 
 Client.create(first_name: "Joseph", last_name: "Lee", email: "jhjlee702@gmail.com", phone_number: "123-456-7890", address: "123 Test St", city: "Testing City", state: "Colorado", zip_code: "12345", gender: 0, DOB: Date.new(1997, 7, 2), emergency_contact_name: "Joanna Lee", emergency_contact_phone: "000-000-0000", drivers_license: "9283993205", insurance: "Statefarm", notes: "This is a test client.")
 
-Case.create(case_number: "000001", client_id: 1, injury_type: "Car Accident", claimant_insurance: "Statefarm", policy_number: "5560429", attorney_assigned: "Adam Bowers", case_status: 0, date_of_incident: Date.new(2024, 11, 11), date_of_retention: Date.new(2024, 11, 15), medical_providers: "N/A", notes: "")
+Case.create(case_number: "000001", client_id: 1, injury_type: "Car Accident", claimant_insurance: "Statefarm", policy_number: "5560429", attorney_assigned: "Adam Bowers", case_status: 0, date_of_incident: Date.new(2024, 11, 11), date_of_retention: Date.new(2024, 11, 15), medical_providers: "N/A", notes: "This is a test case.")

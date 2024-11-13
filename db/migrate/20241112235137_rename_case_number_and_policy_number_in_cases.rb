09afc500b528bf056fa4087d2f3d5e7126e0622a
Joseph Lee <jhjlee702@gmail.com>
2024-11-12 17:01:58 -0700

    correct column names to correct

--- db/migrate/20241112235137_rename_case_number_and_policy_number_in_cases.rb
+++ db/migrate/20241112235137_rename_case_number_and_policy_number_in_cases.rb
@@ -1,6 +1,6 @@
 class RenameCaseNumberAndPolicyNumberInCases < ActiveRecord::Migration[7.1]
   def change
-    rename_column :cases, :case, :case_number
-    rename_column :cases, :policy, :policy_number
+    rename_column :cases, :case_, :case_number
+    rename_column :cases, :policy_, :policy_number
   end
 end
